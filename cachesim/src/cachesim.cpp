748b656e8ee755c8764ece843b0371a6f45a90f0
arthur.almeida.rodrigues <arthur.almeida.rodrigues@23a22076-5c4d-0410-b9f2-21d8122b860c>
2008-05-16 17:43:35 +0000

    Initial import.

---
+++ cachesim/src/cachesim.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cachesim.cpp
+// Author      : Arthur Rodrigues
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
